9c4d30a269abed9a48f472a8448e4fe500b94ae0
DESKTOP-OCUAMNR\user <d2caf@naver.com>
2022-04-27 23:59:01 +0900

    [update] CORS설정 추가, webconfig에

--- src/main/java/com/cloudlibrary/admin/ui/security/WebMvcConfiguration.java
+++ src/main/java/com/cloudlibrary/admin/ui/security/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package com.cloudlibrary.admin.ui.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .excludePathPatterns("/v1/admin/signup")
                 .excludePathPatterns("/v1/admin/findid")
                 .excludePathPatterns("/v1/admin/findpw");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
     }
 }

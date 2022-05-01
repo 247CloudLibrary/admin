f98b1cd3e68d434d7cf7fcae12eae11842b078ce
KIMJISOO <fiapie@kakao.com>
2022-05-01 16:30:27 +0900

    [update] addCorsMappings(WebMvcConfigurer) + CorsConfigurationSource

--- src/main/java/com/cloudlibrary/admin/ui/security/SecurityConfig.java
+++ src/main/java/com/cloudlibrary/admin/ui/security/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -27,8 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic().disable()
-                .csrf().disable();
+                .csrf().disable()
+                .cors();
         http
                 .authorizeRequests()
                 .antMatchers("/v1/admin/health-check").permitAll()
@@ -51,5 +56,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private AuthenticationFilter getAuthenticationFilter() throws Exception {
         AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager(), adminService, env);
         return authenticationFilter;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","OPTIONS","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
--- src/main/java/com/cloudlibrary/admin/ui/security/WebMvcConfiguration.java
+++ src/main/java/com/cloudlibrary/admin/ui/security/WebMvcConfiguration.java
@@ -25,6 +25,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+        registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("*");
     }
 }
